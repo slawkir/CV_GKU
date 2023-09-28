b5ea28e416da34d83646f6aa3552b4801a4c8e95
slawkir <slawkir1984@gmail.com>
2023-09-28 23:37:15 +0400

    feat: add feature callback

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+// variables
+const modalCallBack = document.querySelector('.call-back-window');
+const modalThanks = document.querySelector('.thanks');
+const btnCallBack = document.querySelector('.call-back');
+const modalSubmit = document.querySelector('.modal-form__item button');
+const btnClose = document.querySelector('.btn-close');
+const btnCloseThanks = document.querySelector('.thanks > .modal-content .btn-close');
+
+// function
+function showModalCallBack() { 
+  modalCallBack.style.display = 'flex';
+}
+function closeModalCallBack() { 
+  modalCallBack.style.display = 'none';
+}
+function submitCallBack() { 
+  closeModalCallBack();
+  modalThanks.style.display = 'flex';
+}
+function closemodalThanks() { 
+  modalThanks.style.display = 'none';
+}
+
+
+// events
+btnCallBack.addEventListener("click", showModalCallBack);
+modalSubmit.addEventListener("click", submitCallBack);
+btnClose.addEventListener("click", closeModalCallBack);
+btnCloseThanks.addEventListener("click", closemodalThanks);
