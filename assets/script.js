d353d3a0340291ad3c6584515d5912e2f14a70a8
slawkir <slawkir1984@gmail.com>
2023-09-29 05:56:04 +0400

    feat: add feauture to switch achievements

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,17 @@ const btnCallBack = document.querySelector('.call-back');
 const modalSubmit = document.querySelector('.modal-form__item button');
 const btnClose = document.querySelector('.btn-close');
 const btnCloseThanks = document.querySelector('.thanks > .modal-content .btn-close');
+
+const achievementsItems = document.querySelectorAll('.achievements-item '); 
 const blockProjects = document.querySelector('.projects'); 
 const blockSkills = document.querySelector('.skills'); 
+const blockEducation = document.querySelector('.education'); 
+const blockForeign = document.querySelector('.foreign'); 
+
+const linkProjects = document.querySelector('#link-projects'); 
+const linkSkills = document.querySelector('#link-skills'); 
+const linkEducation = document.querySelector('#link-edu'); 
+const linkForeign = document.querySelector('#link-foreign'); 
 
 
 // function
@@ -23,6 +32,27 @@ function submitCallBack() {
 function closemodalThanks() { 
   modalThanks.style.display = 'none';
 }
+function showBlockSkills() { 
+  hideAchievementsItems();
+  blockSkills.style.display = 'flex';
+}
+function showBlockEducation() { 
+  hideAchievementsItems();
+  blockEducation.style.display = 'flex';
+}
+function showBlockForeign() { 
+  hideAchievementsItems();
+  blockForeign.style.display = 'flex';
+}
+function showBlockProjects() { 
+  hideAchievementsItems();
+  blockProjects.style.display = 'flex';
+}
+function hideAchievementsItems() { 
+  achievementsItems.forEach(item => {
+    item.style.display = "none";
+  });
+}
 
 
 // events
@@ -30,3 +60,8 @@ btnCallBack.addEventListener("click", showModalCallBack);
 modalSubmit.addEventListener("click", submitCallBack);
 btnClose.addEventListener("click", closeModalCallBack);
 btnCloseThanks.addEventListener("click", closemodalThanks);
+
+linkSkills.addEventListener('click', showBlockSkills);
+linkEducation.addEventListener('click', showBlockEducation);
+linkForeign.addEventListener('click', showBlockForeign);
+linkProjects.addEventListener('click', showBlockProjects);
